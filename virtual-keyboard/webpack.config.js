3ff1364081c60684f4026bcc27faa648fbc77a45
-l <djeinwings@gmail.com>
2022-05-04 21:31:51 +0200

    feat: add webpack config and loaders

---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    watchFiles: ['src/template.html'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass)/i,
+        loader: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
