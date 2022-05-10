085e1383c3e2107f0186d100c52a478b3d204044
pdlmn <djeinwings@gmail.com>
2022-05-10 03:18:04 +0200

    fix: remove files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './sass/main.scss';
-import Keyboard from './scripts/Keyboard';
-import Textarea from './scripts/Textarea';
-
-const keyboard = Keyboard(Textarea());
-const keyboardEl = keyboard.create();
-document.body.append(keyboardEl);
--- src/scripts/Textarea.js
+++
@@ -1,9 +0,0 @@
-const Textarea = () => {
-  const textarea = document.createElement('textarea');
-  textarea.classList.add('textarea');
-  textarea.id = 'keyboard-input';
-
-  return textarea;
-};
-
-export default Textarea;
